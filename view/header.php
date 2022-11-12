fa0e1278c93d69ed4891c071a79d039d2335efe6
tuongkhac202 <trankhactuong202@gmail.com>
2022-11-12 16:22:28 +0700

    thêm header trong view

---
+++ view/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dự án 1- nhóm 9</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+    />
+    <link rel="stylesheet" href="view/css/style.css" />
+  </head>
+  <body>
+    <div class="container-fluid p-0 bao">
+      <header>
+        <div class="row m-0 g-2 nav p-1 align-items-center">
+          <div class="col-xl-1 offset-1 d-none d-xl-block logo">
+            <a href="index.php" class="text-decoration-none text-white">
+              <img class="img-fluid" src="upload/logo150.png" alt=""
+            /></a>
+          </div>
+          <div class="row col-xl-10 col-md-12 text-center menu">
+            <div class="row menu">
+              <div class="col text-center">
+                <a class="text-decoration-none text-white" href="index.php"
+                  ><h5>TRANG CHỦ</h5></a
+                >
+              </div>
+              <div class="col text-center">
+                <a
+                  class="text-decoration-none text-white"
+                  href="index.php?act=dl"
+                  ><h5>DU LỊCH</h5></a
+                >
+              </div>
+              <div class="col text-center">
+                <a
+                  class="text-decoration-none text-white"
+                  href="index.php?act=gt"
+                  ><h5>GIỚI THIỆU</h5></a
+                >
+              </div>
+              <div class="col text-center">
+                <a
+                  class="text-decoration-none text-white"
+                  href="index.php?act=lh"
+                  ><h5>LIÊN HỆ</h5></a
+                >
+              </div>
+              <div class="col text-center">
+                <a
+                  class="text-decoration-none text-white"
+                  href="index.php?act=tt"
+                  ><h5>TIN TỨC</h5></a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+         <!-- ảnh banner -->
+         <div class="row banner">
+          <img src="upload/BANNER1.png" alt="" />
+        </div>
+      </header>
