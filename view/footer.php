f4d3aaed003a4c2f2464ac3d844cebdfb79e5b70
baoyenpc03450 <yenttbpc03450@fpt.edu.vn>
2022-11-12 15:52:35 +0700

    Tạo thư mục view ,tạo file footer trong thư mục view

---
+++ view/footer.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css ">
+        <link rel="stylesheet" href="../css/footer.css">
+
+</head>
+<body>
+        
+        <footer>
+                <div class="email_footer">
+                        <img src="../img/email.png" alt="">
+                        <div class="letter_footer">
+                                <b>Đăng ký nhận bản tin</b>
+                                <p>Nhập email để nhận các chương trình khuyến mãi kèm nhiều ưu đãi hấp dẫn </p>
+                        </div>
+                        <div class="input_footer">
+                                <input id="email" type="email" value="email">
+                                <input id="submit" type="submit" value="Đăng ký">
+                        </div>
+
+                </div>
+                <div class="footer">
+                        <img id="logo" src="../img/logo .png" alt="">
+                        <div class="footer_box1">
+                                <h1>Mạng xã hội</h1>
+                                <p><i class="fab fa-facebook-square"></i>&nbsp; Facebook</p>
+                                <p><i class="fab fa-instagram-square"></i>&nbsp; Instagram</p>
+                                <p><i class="fab fa-youtube"></i>&nbsp; Youtube</p>
+                                <p><i class="fab fa-google-plus-square"></i>&nbsp; Google</p>
+
+                        </div>
+                
+                        <div class="footer_box2">
+                                <h1>Liên hệ</h1>
+                                <p>CÔNG TY DỊCH VỤ DU LỊCH YẾN TƯỜNG(YENTUONG TOURIST)</p>
+                                <p>Trụ sở:288 Đ.Nguyễn Văn Linh,Hưng Lợi,Ninh Kiều,TP Cần Thơ</p>
+                                <p>Tel:079.548.3987</p>
+                                <p>Email:yentuong@yentuongrourst.com</p>
+                                <p>Tổng đài:1900 9999</p>
+
+                        </div>
+                </div>
+        </footer>
+</body>
+</html>
