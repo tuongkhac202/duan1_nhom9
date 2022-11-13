3150d60732afb63e8d863683204eddf71948078b
baoyenpc03450 <yenttbpc03450@fpt.edu.vn>
2022-11-13 20:21:58 +0700

    thay đổi đường dẫn giỏ hàng ở header

--- view/header.php
+++ view/header.php
@@ -57,7 +57,7 @@
 
               <div class="col text-center text-white">
                 <a href="index.php?act=dn" class="text-white"><i class="fas fa-user"></i></a> &nbsp; |&nbsp;
-                <a href="index.php?act=dh" class="text-white"><i class="fas fa-shopping-cart"></i></a>
+                <a href="index.php?act=cart" class="text-white"><i class="fas fa-shopping-cart"></i></a>
               </div>
             </div>
           </div>
