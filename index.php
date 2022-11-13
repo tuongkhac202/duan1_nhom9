308c935ab38e0cefe1957fbf63444d030a7a56e4
tuongkhac202 <trankhactuong202@gmail.com>
2022-11-13 12:10:50 +0700

    thêm code điều hướng trong index

--- index.php
+++ index.php
@@ -2,8 +2,23 @@
 session_start();
 include 'dao/pdo.php';
 include 'view/header.php';
-// code dưới này
 
+
+// code dưới này
+if(isset($_GET['act']) && $_GET['act'] !=""){
+    $act = $_GET['act'];
+    switch ($act) {
+        case '':
+            include '';
+            break;
+        
+        default:
+        include 'view/home.php';
+            break;
+    }
+}else{
+    include 'view/home.php';
+}
 
 
 
---
+++ view/home.php
@@ -0,0 +1,371 @@
+<article>
+       
+       <!-- lọc tour -->
+       <div class="row d-none d-xl-block p-3 filter">
+         <div class="row col-xl-8 offset-xl-2 text-center filter-chirld">
+           <form action="" class="row form-filter">
+             <div class="col text-center">
+               <select name="" id="loaitour" class="form-select w-100">
+                 <option value="">Loại tour</option>
+                 <option value="">1</option>
+                 <option value="">2</option>
+               </select>
+             </div>
+             <div class="col text-center">
+               <select name="" id="loaitour" class="form-select w-100">
+                 <option value="">Ngày đi</option>
+                 <option value="">1</option>
+                 <option value="">2</option>
+               </select>
+             </div>
+             <div class="col text-center">
+               <input
+                 type="text"
+                 placeholder="Nơi đến"
+                 class="form-control w-100"
+               />
+             </div>
+             <div class="col text-center">
+               <input
+                 type="text"
+                 placeholder="Giá"
+                 class="form-control w-100"
+               />
+             </div>
+             <div class="col text-center">
+               <input
+                 type="submit"
+                 class="btn btn-danger w-100"
+                 value="Tìm kiếm"
+               />
+             </div>
+           </form>
+         </div>
+       </div>
+
+       <!-- chọn loại tour -->
+       <div class="row d-xl-block d-none p-2 chon-tour">
+         <div class="row col-8 offset-2 gy-1 chon2">
+           <div class="col text-center justify-content-center">
+             <img src="upload/tour-mua-thu.png" alt="" />
+             <h6>TOUR MÙA THU</h6>
+           </div>
+           <div class="col text-center justify-content-center">
+             <img src="upload/tour-tet.png" alt="" />
+             <h6>TOUR TẾT</h6>
+           </div>
+           <div class="col text-center justify-content-center">
+             <img src="upload/tour-hh.png" alt="" />
+             <h6>TOUR HÀNH HƯƠNG</h6>
+           </div>
+           <div class="col text-center justify-content-center">
+             <img src="upload/tour-world.png" alt="" />
+             <h6>WORLD</h6>
+           </div>
+         </div>
+       </div>
+
+       <!-- tour bắc -->
+       <div class="mien mien-bac">
+         <div class="contentt text-center text-white">
+           <p class="pt-2 mb-1">
+             Khám phá lịch sử, văn hóa, con người miền Nam với
+           </p>
+           <h5 class="pb-2 m-0">TOUR MIỀN BẮC</h5>
+         </div>
+
+         <div class="row m-0 py-5 px-5 gx-xl-5 tour justify-content-center">
+
+           <div class="row m-0 col-12 rounded-top rounded-bottom col-xl-3 tourr">
+             <div class="col-12 col-xl-12 rounded-top image-tour px-0 ">
+               <a href="">
+                 <img
+                 class="img-fluid rounded-top"
+                 src="upload/43362.jpg"
+                 alt=""
+               />
+                </a>
+             </div>
+             <div class="content rounded-bottom bg-light p-3">
+               <div class="title-tour text-center">
+                 <h6>Du lịch sóc trăng</h6>
+               </div>
+               <div class="add-time justify-content-start">
+                 <p><i class="far fa-flag"></i>&ensp; Hồ Chí Minh</p>
+                 <p><i class="far fa-clock"></i> &ensp;24/11/2022</p>
+               </div>
+               <div class="row m-0 footer-tour">
+                 <div class="col px-0 d-flex justify-content-center align-items-center "><i class="far fa-money-bill-alt"></i>&ensp; <p class="text-danger m-0">8.000.000 đ</p></div>
+                 <div class="col text-center">
+                   <button class="btn btn-danger"><a href="" class="text-white text-decoration-none">ĐẶT LỊCH</a></button>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="row m-0 col-12 rounded-top rounded-bottom col-xl-3  tourr">
+             <div class="col-12 col-xl-12 rounded-top image-tour px-0">
+               <a href="">
+                 <img
+                 class="img-fluid rounded-top"
+                 src="upload/43362.jpg"
+                 alt=""
+               />
+                </a>
+             </div>
+             <div class="content rounded-bottom bg-light p-3">
+               <div class="title-tour text-center">
+                 <h6>Du lịch sóc trăng</h6>
+               </div>
+               <div class="add-time justify-content-start">
+                 <p><i class="far fa-flag"></i>&ensp; Hồ Chí Minh</p>
+                 <p><i class="far fa-clock"></i> &ensp;24/11/2022</p>
+               </div>
+               <div class="row m-0 footer-tour">
+                 <div class="col px-0 d-flex justify-content-center align-items-center d-flex "><i class="far fa-money-bill-alt"></i>&ensp; <p class="text-danger m-0">8.000.000 đ</p></div>
+                 <div class="col text-center">
+                   <button class="btn btn-danger"><a href="" class="text-white text-decoration-none">ĐẶT LỊCH</a></button>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="row m-0 col-12 rounded-top rounded-bottom col-xl-3 tourr">
+             <div class="col-12 col-xl-12 rounded-top image-tour px-0">
+              <a href="">
+               <img
+               class="img-fluid rounded-top"
+               src="upload/43362.jpg"
+               alt=""
+             />
+              </a>
+             </div>
+             <div class="content rounded-bottom bg-light p-3">
+               <div class="title-tour text-center">
+                 <h6>Du lịch sóc trăng</h6>
+               </div>
+               <div class="add-time justify-content-start">
+                 <p><i class="far fa-flag"></i>&ensp; Hồ Chí Minh</p>
+                 <p><i class="far fa-clock"></i> &ensp;24/11/2022</p>
+               </div>
+               <div class="row m-0 footer-tour">
+                 <div class="col px-0 d-flex justify-content-center align-items-center d-flex "><i class="far fa-money-bill-alt"></i>&ensp; <p class="text-danger m-0">8.000.000 đ</p></div>
+                 <div class="col text-center">
+                   <button class="btn btn-danger"><a href="" class="text-white text-decoration-none">ĐẶT LỊCH</a></button>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+         </div>
+       </div>
+
+
+
+        <!-- tour trung -->
+        <div class="mien mien-trung">
+         <div class="contentt text-center text-white">
+           <p class="pt-2 mb-1">
+             Khám phá lịch sử, văn hóa, con người miền Nam với
+           </p>
+           <h5 class="pb-2 m-0">TOUR MIỀN TRUNG</h5>
+         </div>
+
+         <div class="row m-0 py-5 px-5 gx-xl-5 tour justify-content-center">
+
+           <div class="row m-0 col-12 rounded-top rounded-bottom col-xl-3 tourr">
+             <div class="col-12 col-xl-12 rounded-top image-tour px-0 ">
+               <a href="">
+                 <img
+                 class="img-fluid rounded-top"
+                 src="upload/43362.jpg"
+                 alt=""
+               />
+                </a>
+             </div>
+             <div class="content rounded-bottom bg-light p-3">
+               <div class="title-tour text-center">
+                 <h6>Du lịch sóc trăng</h6>
+               </div>
+               <div class="add-time justify-content-start">
+                 <p><i class="far fa-flag"></i>&ensp; Hồ Chí Minh</p>
+                 <p><i class="far fa-clock"></i> &ensp;24/11/2022</p>
+               </div>
+               <div class="row m-0 footer-tour">
+                 <div class="col px-0 d-flex justify-content-center align-items-center d-flex "><i class="far fa-money-bill-alt"></i>&ensp; <p class="text-danger m-0">8.000.000 đ</p></div>
+                 <div class="col text-center">
+                   <button class="btn btn-danger"><a href="" class="text-white text-decoration-none">ĐẶT LỊCH</a></button>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="row m-0 col-12 rounded-top rounded-bottom col-xl-3  tourr">
+             <div class="col-12 col-xl-12 rounded-top image-tour px-0">
+               <a href="">
+                 <img
+                 class="img-fluid rounded-top"
+                 src="upload/43362.jpg"
+                 alt=""
+               />
+                </a>
+             </div>
+             <div class="content rounded-bottom bg-light p-3">
+               <div class="title-tour text-center">
+                 <h6>Du lịch sóc trăng</h6>
+               </div>
+               <div class="add-time justify-content-start">
+                 <p><i class="far fa-flag"></i>&ensp; Hồ Chí Minh</p>
+                 <p><i class="far fa-clock"></i> &ensp;24/11/2022</p>
+               </div>
+               <div class="row m-0 footer-tour">
+                 <div class="col px-0 d-flex justify-content-center align-items-center d-flex "><i class="far fa-money-bill-alt"></i>&ensp; <p class="text-danger m-0">8.000.000 đ</p></div>
+                 <div class="col text-center">
+                   <button class="btn btn-danger"><a href="" class="text-white text-decoration-none">ĐẶT LỊCH</a></button>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="row m-0 col-12 rounded-top rounded-bottom col-xl-3 tourr">
+             <div class="col-12 col-xl-12 rounded-top image-tour px-0">
+              <a href="">
+               <img
+               class="img-fluid rounded-top"
+               src="upload/43362.jpg"
+               alt=""
+             />
+              </a>
+             </div>
+             <div class="content rounded-bottom bg-light p-3">
+               <div class="title-tour text-center">
+                 <h6>Du lịch sóc trăng</h6>
+               </div>
+               <div class="add-time justify-content-start">
+                 <p><i class="far fa-flag"></i>&ensp; Hồ Chí Minh</p>
+                 <p><i class="far fa-clock"></i> &ensp;24/11/2022</p>
+               </div>
+               <div class="row m-0 footer-tour">
+                 <div class="col px-0 d-flex justify-content-center align-items-center d-flex "><i class="far fa-money-bill-alt"></i>&ensp; <p class="text-danger m-0">8.000.000 đ</p></div>
+                 <div class="col text-center">
+                   <button class="btn btn-danger"><a href="" class="text-white text-decoration-none">ĐẶT LỊCH</a></button>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+         </div>
+       </div>
+
+
+
+
+        <!-- tour nam-->
+        <div class="mien mien-nam">
+         <div class="contentt text-center text-white">
+           <p class="pt-2 mb-1">
+             Khám phá lịch sử, văn hóa, con người miền Nam với
+           </p>
+           <h5 class="pb-2 m-0">TOUR MIỀN NAM</h5>
+         </div>
+
+         <div class="row m-0 py-5 px-5 gx-xl-5 tour justify-content-center">
+
+           <div class="row m-0 col-12 rounded-top rounded-bottom col-xl-3 tourr">
+             <div class="col-12 col-xl-12 rounded-top image-tour px-0 ">
+               <a href="">
+                 <img
+                 class="img-fluid rounded-top"
+                 src="upload/43362.jpg"
+                 alt=""
+               />
+                </a>
+             </div>
+             <div class="content rounded-bottom bg-light p-3">
+               <div class="title-tour text-center">
+                 <h6>Du lịch sóc trăng</h6>
+               </div>
+               <div class="add-time justify-content-start">
+                 <p><i class="far fa-flag"></i>&ensp; Hồ Chí Minh</p>
+                 <p><i class="far fa-clock"></i> &ensp;24/11/2022</p>
+               </div>
+               <div class="row m-0 footer-tour">
+                 <div class="col px-0 d-flex justify-content-center align-items-center d-flex "><i class="far fa-money-bill-alt"></i>&ensp; <p class="text-danger m-0">8.000.000 đ</p></div>
+                 <div class="col text-center">
+                   <button class="btn btn-danger"><a href="" class="text-white text-decoration-none">ĐẶT LỊCH</a></button>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="row m-0 col-12 rounded-top rounded-bottom col-xl-3  tourr">
+             <div class="col-12 col-xl-12 rounded-top image-tour px-0">
+               <a href="">
+                 <img
+                 class="img-fluid rounded-top"
+                 src="upload/43362.jpg"
+                 alt=""
+               />
+                </a>
+             </div>
+             <div class="content rounded-bottom bg-light p-3">
+               <div class="title-tour text-center">
+                 <h6>Du lịch sóc trăng</h6>
+               </div>
+               <div class="add-time justify-content-start">
+                 <p><i class="far fa-flag"></i>&ensp; Hồ Chí Minh</p>
+                 <p><i class="far fa-clock"></i> &ensp;24/11/2022</p>
+               </div>
+               <div class="row m-0 footer-tour">
+                 <div class="col px-0 d-flex justify-content-center align-items-center d-flex "><i class="far fa-money-bill-alt"></i>&ensp; <p class="text-danger m-0">8.000.000 đ</p></div>
+                 <div class="col text-center">
+                   <button class="btn btn-danger"><a href="" class="text-white text-decoration-none">ĐẶT LỊCH</a></button>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="row m-0 col-12 rounded-top rounded-bottom col-xl-3 tourr">
+             <div class="col-12 col-xl-12 rounded-top image-tour px-0">
+              <a href="">
+               <img
+               class="img-fluid rounded-top"
+               src="upload/43362.jpg"
+               alt=""
+             />
+              </a>
+             </div>
+             <div class="content rounded-bottom bg-light p-3">
+               <div class="title-tour text-center">
+                 <h6>Du lịch sóc trăng</h6>
+               </div>
+               <div class="add-time justify-content-start">
+                 <p><i class="far fa-flag"></i>&ensp; Hồ Chí Minh</p>
+                 <p><i class="far fa-clock"></i> &ensp;24/11/2022</p>
+               </div>
+               <div class="row m-0 footer-tour">
+                 <div class="col px-0 d-flex justify-content-center align-items-center d-flex "><i class="far fa-money-bill-alt"></i>&ensp; <p class="text-danger m-0">8.000.000 đ</p></div>
+                 <div class="col text-center">
+                   <button class="btn btn-danger"><a href="" class="text-white text-decoration-none">ĐẶT LỊCH</a></button>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+         </div>
+       </div>
+
+        <div class="row m-0 py-4  khoanh-khac-lu-hanh justify-content-center">
+<div class="col-12 col-xl-12 p-4 text-center lu-hanh-title">
+ <h3 class="text-white">KHOẢNH KHẮC LỮ HÀNH</h3>
+</div>
+<div class="row col-12 col-xl-10 g-4  m-0 anh-lu-hanh">
+<div class="col-6 col-xl-4"><img class="img-fluid" src="upload/lu-hanh-1.png" alt=""></div>
+<div class="col-6 col-xl-4"><img class="img-fluid" src="upload/lu-hanh-2.png" alt=""></div>
+<div class="col-6 col-xl-4"><img class="img-fluid" src="upload/lu-hanh-3.png" alt=""></div>
+<div class="col-6 col-xl-4"><img class="img-fluid" src="upload/lu-hanh-4.png" alt=""></div>
+<div class="col-6 col-xl-4"><img class="img-fluid" src="upload/lu-hanh-5.png" alt=""></div>
+<div class="col-6 col-xl-4"><img class="img-fluid" src="upload/lu-hanh-6.png" alt=""></div>
+</div>
+        </div>
+     </article>
