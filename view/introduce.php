5badf596c43c962ef9452c50251df21e6a5fd51e
tuongnk <117510443+tuongnk@users.noreply.github.com>
2022-11-15 20:21:12 +0700

    gioithieu

--- view/introduce.php
+++ view/introduce.php
@@ -1,13 +1,13 @@
 <article>
-    <div class="row m-0">
+    <div class="row m-0  text-center">
         <div class="col-12">
         <h1>Giới Thiệu</h1>
         </div>
-        <div class="row col-12 py-5 baoimg">
-            <div class="col-3 img"><img src="upload/logo.png" alt=""></div>
-            <div class="col-9">
-                <h3 class="font-thu-phap">Thư Ngõ</h3>
-                <hr class="color-dark">
+        <div class="row col-8 offset-2 py-5 baoimg">
+            <div class="col-3 img mx-3"><img src="upload/logo150.png" alt=""></div>
+            <div class="row col-9 openletter">
+                <h1 class="col-12 font-thu-phap ">Thư Ngõ</h1>
+                <hr class="col-12 color-dark w-100">
             </div>
         </div>
 
