9fdf457a2239872b2a51b8da6e053da4769e4779
tuongkhac202 <trankhactuong202@gmail.com>
2022-11-15 16:28:36 +0700

    up thống kê

---
+++ dao/thong-ke.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'pdo.php';
+
+function thong_ke_tour(){
+    $sql = "select tours.id_tour, category, bookings.id_booking, "
+            ."count(category) so_luong,"
+            ."count(bookings.id_booking) so_luot,"
+            ."name_place in (select max(tours.id_tour)  from tours join bookings on tours.id_tour=bookings.id_tour group by category),"
+            ."min(price) gia_min,"
+            ."max(price) gia_max "
+            ." from tours join bookings on tours.id_tour=bookings.id_tour "
+            ."group by tours.id_tour, category";
+    return pdo_query($sql);
+}
+
+function thong_ke_binh_luan(){
+    $sql = "SELECT hh.ma_hh, hh.ten_hh,"
+            . " COUNT(*) so_luong,"
+            . " MIN(bl.ngay_bl) cu_nhat,"
+            . " MAX(bl.ngay_bl) moi_nhat"
+            . " FROM binh_luan bl "
+            . " JOIN hang_hoa hh ON hh.ma_hh=bl.ma_hh "
+            . " GROUP BY hh.ma_hh, hh.ten_hh"
+            . " HAVING so_luong > 0";
+    return pdo_query($sql);
+}
