66312fc15cc84554b06efacec558327177b9426e
baoyenpc03450 <yenttbpc03450@fpt.edu.vn>
2022-11-26 17:03:34 +0700

    [dao]handbook-thêm hàm

--- dao/handbook.php
+++ dao/handbook.php
@@ -1,15 +1,26 @@
 <?php
-function insert_handbook($id_handbook,$title,$content,$img){
-        $sql = "insert into handbooks(id_handbook,title,content,img) values ('$id_handbook','$title','$content','$img')";
-        pdo_execute($sql);
-        
-    }
-function load_handbook_one($id){
-        $sql ="select* from handbooks where id_handbook='$id'";
-        return pdo_query($sql);
-    }
-    function load_handbook_all(){
-        $sql ="select* from handbooks";
-        return pdo_query($sql);
-    }
-
+function insert_handbook($id_handbook, $title, $content, $img)
+{
+    $sql = "insert into handbooks(id_handbook,title,content,img) values ('$id_handbook','$title','$content','$img')";
+    pdo_execute($sql);
+}
+function load_handbook_one($id)
+{
+    $sql = "select* from handbooks where id_handbook='$id'";
+    return pdo_query($sql);
+}
+function load_handbook_all()
+{
+    $sql = "select* from handbooks limit 0,2";
+    return pdo_query($sql);
+}
+function load_handbook_all1()
+{
+    $sql = "select* from handbooks limit 2,8";
+    return pdo_query($sql);
+}
+function load_handbook_all2()
+{
+    $sql = "select* from handbooks limit 8,13";
+    return pdo_query($sql);
+}
