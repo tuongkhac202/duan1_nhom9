cca90ad18c42f55ca40421caabf2b3c1d2120c8e
baoyenpc03450 <yenttbpc03450@fpt.edu.vn>
2022-11-26 15:39:35 +0700

    [dao]handbook-tối ưu hàm

--- dao/handbook.php
+++ dao/handbook.php
@@ -1,11 +1,15 @@
 <?php
 function insert_handbook($id_handbook,$title,$content,$img){
-        $sql = "insert into handbook(id_handbook,title,content,img) values ('$id_handbook','$title','$content','$img')";
+        $sql = "insert into handbooks(id_handbook,title,content,img) values ('$id_handbook','$title','$content','$img')";
         pdo_execute($sql);
         
     }
 function load_handbook_one($id){
-        $sql ="select* from handbook where id_handbook='$id'";
-        $hand_book = pdo_query($sql);
-        return $hand_book;
+        $sql ="select* from handbooks where id_handbook='$id'";
+        return pdo_query($sql);
     }
+    function load_handbook_all(){
+        $sql ="select* from handbooks";
+        return pdo_query($sql);
+    }
+
