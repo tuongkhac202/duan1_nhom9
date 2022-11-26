387c6ab85f6fe58ffd9e7264deafec4f1fd1be7d
baoyenpc03450 <yenttbpc03450@fpt.edu.vn>
2022-11-26 17:08:25 +0700

    [view]handbook-load handbook

--- view/handbook.php
+++ view/handbook.php
@@ -3,80 +3,63 @@
                 <h1 class="text-danger">CẨM NANG DU LỊCH</h1>
 
         </div>
+        <div class="row">
+        <div class="row col-8">
+       
         <?php
         $handbook = load_handbook_all();
         foreach ($handbook as $handbooks) {
-          extract($handbooks);
-          $hinh_handbook = "upload/" . $img;
-          echo'<div class="row">
-          <div class="col-8">
-                        <div class="col-4 m-5">
-                                <img class="rounded " src="'.$hinh_handbook.'">
+                extract($handbooks);
+                $hinh_handbook = "upload/" . $img;
+                echo '
+       
+                        <div class="col-12 m-5">
+                                <img class="rounded  "  src="' . $hinh_handbook . '" width="800" height="400">
                                 <p class=" text-danger">Cẩm nang du lịch</p>
-                                <h4 class="text-info"><a href="index.php?act=handbook&id=">'.$title.'</a></h4>
+                                <h4 class="text-info"><a href="index.php?act=handbook&id="></a>' . $title . '</a></h4>
                         </div>
-                </div>
-          </div>';
-?>
-        <div class="row">
-                <div class="col-8">
-                        <div class="col-4 m-5">
-                                <img class="rounded " src="upload/khoanh-khac-lu-hanh.png" alt="" width="800" height="400">
-                                <p class=" text-danger">Cẩm nang du lịch</p>
-                                <h4 class="text-info"><a href="index.php?act=handbook&id=">18 mẹo tiết kiệm tiền đi du lịch dễ dàng hơn lúc nào hết</h4>
-                        </div>
-                </div>
-                <div class="col-4 ">
-                        <div class="col-4 m-5">
-                                <img class="rounded" src="upload/mien-bac-bg.png" alt="" width="300">
-                                <p class=" text-danger">Cẩm nang du lịch</p>
-                                <h6 class="text-info">Những lưu ý dành cho khách du lịch Tây Bắc lần đầu</h6>
-                        </div>
-                        <div class="col-4 m-5">
-                                <img class="rounded" src="upload/mien-bac-bg.png" alt="" width="300">
-                                <p class=" text-danger">Cẩm nang du lịch</p>
-                                <h6 class="text-info">Những lưu ý dành cho khách du lịch Tây Bắc lần đầu</h6>
-                        </div>
-                        <div class="col-4 m-5">
-                                <img class="rounded" src="upload/mien-bac-bg.png" alt="" width="300">
-                                <p class=" text-danger">Cẩm nang du lịch</p>
-                                <h6 class="text-info">Những lưu ý dành cho khách du lịch Tây Bắc lần đầu</h6>
-                        </div>
-                </div>
+              
+                
+        ';
+        }
+        
+        ?>
+          </div>
+          <div class="col-4 ">
+        <?php
+        $handbook = load_handbook_all1();
+        foreach ($handbook as $handbooks) {
+                extract($handbooks);
+                $hinh_handbook = "upload/" . $img;
+                echo '
+   <div class="col-4 m-5">
+           <img class="rounded" src="' . $hinh_handbook . '" alt="" width="300">
+           <p class=" text-danger">Cẩm nang du lịch</p>
+           <h6 class="text-info"><a href="index.php?act=handbook&id="></a>' . $title . '</h6>
+   </div>
+';
+        }
+        ?>
         </div>
-        <hr class="text-primary">
-        <div class="row">
-                <div class=" col-4 m-5">
-                        <img class="rounded" src="upload/khoanh-khac-lu-hanh.png" alt="" width="500">
-                        <p class=" text-danger">Cẩm nang du lịch</p>
-                </div>
-                <div class="col-5 m-5">
-                        <h4 class="text-info">Du lịch Đà Lạt với cẩm nang đầy đủ và súc tích nhất. Cẩm nang du lịch Đà Lạt giới thiệu các điểm đến thú vị, địa điểm ăn uống và nhiều thông tin bổ ích để bạn tham khảo trước khi đến thành phố hoa xinh đẹp này.</h4>
-                </div>
-
+        <?php
+         $handbook = load_handbook_all2();
+         foreach ($handbook as $handbooks) {
+                 extract($handbooks);
+                 $hinh_handbook = "upload/" . $img;
+                 echo'
+                 <hr class="text-primary">
+                 <div class="row">
+                         <div class=" col-4 m-5">
+                                 <img class="rounded" src="'.$hinh_handbook.'" alt="" width="500">
+                                 <p class=" text-danger">Cẩm nang du lịch</p>
+                         </div>
+                         <div class="col-5 m-5">
+                                 <h4 class="text-info"><a href="index.php?act=handbook&id="></a>' . $title .'</h4>
+                         </div>
+         
+                 </div>';
+         }
+        ?>
         </div>
-        <hr class="text-primary">
-        <div class="row">
-                <div class=" col-4 m-5">
-                        <img class="rounded" src="upload/khoanh-khac-lu-hanh.png" alt="" width="500">
-                        <p class=" text-danger">Cẩm nang du lịch</p>
-                </div>
-                <div class="col-5 m-5">
-                        <h4 class="text-info">Du lịch Đà Lạt với cẩm nang đầy đủ và súc tích nhất. Cẩm nang du lịch Đà Lạt giới thiệu các điểm đến thú vị, địa điểm ăn uống và nhiều thông tin bổ ích để bạn tham khảo trước khi đến thành phố hoa xinh đẹp này.</h4>
-                </div>
-
-        </div>
-        <hr class="text-primary">
-        <div class="row">
-                <div class=" col-4 m-5">
-                        <img class="rounded" src="upload/khoanh-khac-lu-hanh.png" alt="" width="500">
-                        <p class=" text-danger">Cẩm nang du lịch</p>
-                </div>
-                <div class="col-5 m-5">
-                        <h4 class="text-info">Du lịch Đà Lạt với cẩm nang đầy đủ và súc tích nhất. Cẩm nang du lịch Đà Lạt giới thiệu các điểm đến thú vị, địa điểm ăn uống và nhiều thông tin bổ ích để bạn tham khảo trước khi đến thành phố hoa xinh đẹp này.</h4>
-                </div>
-
-        </div>
-
 
 </article>
