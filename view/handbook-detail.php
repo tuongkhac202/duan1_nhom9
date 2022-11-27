effeda45c98fce116cdd9f01a4acb123ba64f591
baoyenpc03450 <yenttbpc03450@fpt.edu.vn>
2022-11-27 19:33:31 +0700

    [view]handbook-detail-thêm cơ sở dữ liệu

--- view/handbook-detail.php
+++ view/handbook-detail.php
@@ -1,13 +1,14 @@
 <article>
+        <?php
+        foreach ($handbook_this as $hand) {;
+        }
+        ?>
         <div class="row m-2">
-                <h2 class="text-danger text-center m-1">18 mẹo tiết kiệm tiền đi du lịch dễ dàng hơn lúc nào hết </h2>
+                <h2 class="text-danger text-center m-1"><?php echo $hand['title'] ?> </h2>
                 <div class="col-8 justify-content-center mx-auto">
-                        <img class="mx-auto d-block img-fluid" src="upload/AdobeStock_253835138_Preview.jpeg" alt="" width="500" height="200"><br>
+                        <?php echo '<img src="upload/' . $hand["img"] . '" alt="" class="mx-auto d-block img-fluid">' ?>
+                        <p><?php echo $hand['content'] ?></p>
 
-                        Sáng:
-                        Xe và HDV đón Quý khách tại khách sạn bắt đầu hành trình khám phá TP. Hạ Long xinh đẹp. Địa điểm đầu tiên, Quý khách tới tham quan Bảo tàng và Thư Viện Quảng Ninh – thiết kế kiến trúc độc đáo đoạt giải thưởng Công trình của năm 2013. Trưa: Xe đến Vịnh Hạ Long. Quý khách lên tàu, nghỉ ngơi và thưởng thức bữa trưa. - Sau bữa trưa, Quý khách sẽ được hoạt động & tham quan các điểm chính sau: (Ngoài ra còn nhiều đảo đá chưa được đặt tên khác trên Vịnh) - Động Thiên Cung – một trong những động đẹp nhất của Vịnh Hạ Long - Hòn Chó Đá, hòn Đỉnh Hương, hòn Gà Chọi,… - Chèo thuyền kayak hoặc đi thuyền nan thăm khu vực Làng chài Ba Hang (phụ phí 90.000 VND)
-                        Chiều:
-                        Tàu cập bến, Quý khách lên xe về khách sạn. Trên đường về Quý khách dừng chân trên cầu Bãi Cháy – một trong 10 cây cầu đẹp nhất Việt Nam, chiêm ngưỡng và chụp ảnh lưu niệm cùng cây cầu và vòng quay Mặt trời Sun Wheel. Xe về đến khách sạn kết thúc chương trình Tối: xe và hướng dẫn viên đưa du khách trở về điểm hẹn ban đầu. Chia tay du khách, kết thúc chương trình tour du lịch Sóc Trăng 1 ngày.
                 </div>
 
 
