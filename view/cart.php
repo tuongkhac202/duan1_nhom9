4153ef73381259b1901ceef022f2d68027059fe7
tuongkhac202 <trankhactuong202@gmail.com>
2022-11-29 12:36:46 +0700

    [front-end] cart.php - fix css

--- view/cart.php
+++ view/cart.php
@@ -3,36 +3,50 @@
                 <div class="row">
                         <div class="col-6">
 
-                                <div class=" w-75 h-20 border m-3 bg-light">
+                                <div class="py-2 m-3 bg-light">
+
                                         <h3 class="text-center color-main ">Thông tin người đặt</h3>
                                 </div>
-                                <h4 class="text-danger">Tổng:</h4>
-                                <div class="col-4">
-                                        <input class="rounded m-3 form-control" type="text" placeholder="Họ và tên">
-                                        <input class="rounded m-3 form-control" type="phone" placeholder="Số điện thoại">
-                                        <input class="rounded m-3 form-control" type="email" placeholder="Email">
-                                        <input class="rounded m-3 form-control" type="text" placeholder="Mã giảm giá">
+                                <div class="py-2 m-3 bg-light px-4 py-5"> <br><input class="rounded form-control" type="text" placeholder="Họ và tên">
+                                        <br><input class="rounded  form-control" type="phone" placeholder="Số điện thoại">
+                                        <br><input class="rounded  form-control" type="email" placeholder="Email">
+                                        <br><input class="rounded  form-control" type="text" placeholder="Mã giảm giá">
                                         <br>
-                                        <input class="m-3" type="radio" value="">Thanh toán tại văn phòng
-                                        <br>
-                                        <input class="m-3" type="radio" value="">Chuyển khoản
-
+                                        <input class="" type="radio" value=""> Thanh toán tại văn phòng
+                                        <br><br> <input class="" type="radio" value=""> Chuyển khoản
                                 </div>
+
+
+
                         </div>
                         <div class="col-6">
-                                <div class=" w-75 h-20 border m-3 bg-light">
+                                <div class="py-2 m-3 bg-light">
                                         <h3 class="text-center color-main">Thông tin dịch vụ</h3>
                                 </div>
                                 <div class="m-3">
-                                        <img class="rounded" src="upload/khoanh-khac-lu-hanh.png" alt="" width="300">
+                                        <img class="rounded w-100" src="upload/khoanh-khac-lu-hanh.png" alt="" width="300">
                                         <h6 class="text-danger">Du Lịch Ninh Bình - Hạ Long: Tuyệt Tình Cốc - Bái Đính - Tràng An - Hạ Long - Yên Tử</h6>
                                         <b>Ngày khởi hành:20/11/2022</b>
                                         <br>
                                         <br>
-                                        <b>Số vé:</b><input class=" w-25 form-control" type="number">
+                                        <b>Số người:</b>
+                                        <input class=" w-25 form-control" type="number">
+                                        <br>
+                                        <b>Ghi chú:</b>
+                                        <br>
+                                        <textarea class="w-100" name="" id="" cols="30" rows="10"></textarea>
+                                        <br>
+                                        <b>Giá tour:</b>
+                                        <br>
+                                        <b>Trạng thái:</b>
+                                        <br>
+                                        <center><button type="button" class=" btn btn-danger m-5">Xóa</button></center>
+                                        <hr>
+                                        <h4 class="text-danger">Tổng tiền:</h4>
+                                        <center><button type="button" class=" btn btn-success m-5">Thanh toán</button></center>
 
                                 </div>
-                                <button type="button" class=" btn btn-danger m-5">Thanh toán</button>
+
                         </div>
 
 
