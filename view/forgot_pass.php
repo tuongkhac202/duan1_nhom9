8e4a8a81d31f8585d5010cd08c7301d95eb7b0ae
baoyenpc03450 <yenttbpc03450@fpt.edu.vn>
2022-11-29 20:32:04 +0700

    [view]forgot_pass - thêm method

---
+++ view/forgot_pass.php
@@ -0,0 +1,5 @@
+<form class="" action="index.php?act=forgot_password" method="post">
+        <h2 class="text-danger">Quên mật khẩu</h2>
+        <input type="email" name="email">
+        <input type="submit" name="send_code" value="gui">
+</form>
