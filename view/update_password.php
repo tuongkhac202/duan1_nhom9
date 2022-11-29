ee34eb57a6c2f18186422270f509dcd336cba9a9
baoyenpc03450 <yenttbpc03450@fpt.edu.vn>
2022-11-29 20:33:57 +0700

    [view]update_password-xây dựng chức năng quên mật khẩu

---
+++ view/update_password.php
@@ -0,0 +1,7 @@
+<form action="index.php?act=update_password" method="post">
+        <input type="text" name="code">
+        <input type="password" name="new_password">
+        <input type="password" name="renew_password">
+        <input type="submit" name="update_pass" value="update_password">
+
+</form>
