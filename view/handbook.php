841bb24dc1da5e07d767bbbc6f72e179bf480b47
tuongkhac202 <trankhactuong202@gmail.com>
2022-11-30 18:58:44 +0700

    [handbook.php] fix handbook layout

--- view/handbook.php
+++ view/handbook.php
@@ -3,8 +3,8 @@
                 <h1 class="text-danger">CẨM NANG DU LỊCH</h1>
 
         </div>
-        <div class="row px-5 gy-4">
-                <div class="row col-8 py-0 px-5 m-0">
+        <div class="row px-5 gx-5">
+                <div class="row col-8 ">
                         <?php
                         $handbook = load_handbook_all();
                         foreach ($handbook as $handbooks) {
@@ -21,10 +21,25 @@
                         }
                         ?>
 
-                </div>
-          </div>';    
-        }
-?>
+                </div>   
+                <div class="row col-4 ">
+                        <?php
+                        $handbook = load_handbook_all1();
+                        foreach ($handbook as $handbooks) {
+                                extract($handbooks);
+                                $hinh_handbook = "upload/" . $img;
+                                echo '
+       
+                        <div class="col-12">
+                                <img class="rounded  "  src="' . $hinh_handbook . '" width="100%" height="200">
+                                <p class=" text-danger">Cẩm nang du lịch</p>
+                                <h4 class="text-info "><a class="text-decoration-none" href="index.php?act=handbook-detail&id=' . $id_handbook . '">' . $title . '</a></h4>
+                        </div>
+                ';
+                        }
+                        ?>
+
+                </div>   
         <?php
          $handbook = load_handbook_all2();
          foreach ($handbook as $handbooks) {
