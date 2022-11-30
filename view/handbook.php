ef85c314a089e7945b29d681ba21f434869ec6d1
tuongkhac202 <trankhactuong202@gmail.com>
2022-11-30 16:36:23 +0700

    [handbook] fix-layout-handbook

--- view/handbook.php
+++ view/handbook.php
@@ -3,9 +3,8 @@
                 <h1 class="text-danger">CẨM NANG DU LỊCH</h1>
 
         </div>
-        <div class="row">
-                <div class="row col-8">
-
+        <div class="row px-5 gy-4">
+                <div class="row col-8 py-0 px-5 m-0">
                         <?php
                         $handbook = load_handbook_all();
                         foreach ($handbook as $handbooks) {
@@ -13,23 +12,49 @@
                                 $hinh_handbook = "upload/" . $img;
                                 echo '
        
-                        <div class="col-12 m-5">
-                                <img class="rounded  "  src="' . $hinh_handbook . '" width="800" height="400">
+                        <div class="col-12">
+                                <img class="rounded  "  src="' . $hinh_handbook . '" width="100%" height="400">
                                 <p class=" text-danger">Cẩm nang du lịch</p>
                                 <h4 class="text-info "><a class="text-decoration-none" href="index.php?act=handbook-detail&id=' . $id_handbook . '">' . $title . '</a></h4>
                         </div>
+                ';
+                        }
+                        ?>
+
                 </div>
-          </div>';    
-        }
-?>
-        <?php
-         $handbook = load_handbook_all2();
-         foreach ($handbook as $handbooks) {
-                 extract($handbooks);
-                 $hinh_handbook = "upload/" . $img;
-                 echo'
+                
+
+                <div class="row col-4 py-0 px-5 m-0">
+                        <?php
+                        $handbook = load_handbook_all1();
+                        foreach ($handbook as $handbooks) {
+                                extract($handbooks);
+                                $hinh_handbook = "upload/" . $img;
+                                echo '
+       
+                        <div class="col-12">
+                                <img class="rounded  "  src="' . $hinh_handbook . '" width="100%" height="200">
+                                <p class=" text-danger">Cẩm nang du lịch</p>
+                                <h4 class="text-info "><a class="text-decoration-none" href="index.php?act=handbook-detail&id=' . $id_handbook . '">' . $title . '</a></h4>
+                        </div>
+                ';
+                        }
+                        ?>
+
+                </div>
+
+
+
+                
+
+                <?php
+                $handbook = load_handbook_all2();
+                foreach ($handbook as $handbooks) {
+                        extract($handbooks);
+                        $hinh_handbook = "upload/" . $img;
+                        echo '
                  <hr class="text-primary">
-                 <div class="row">
+                 <div class="row col-12">
                          <div class=" col-4 m-5">
                                  <img class="rounded" src="' . $hinh_handbook . '" alt="" width="500">
                                  <p class=" text-danger">Cẩm nang du lịch</p>
@@ -37,10 +62,9 @@
                          <div class="col-5 m-5">
                                  <h4 class="text-info"><a class="text-decoration-none" href="index.php?act=handbook-detail&id=' . $id_handbook . '"></a>' . $title . '</h4>
                          </div>
-         
                  </div>';
-                        }
-                        ?>
-                </div>
+                }
+                ?>
+        </div>
 
 </article>
