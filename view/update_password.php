91cea028a90dc6f6be301a96680e90669b6a4104
baoyenpc03450 <yenttbpc03450@fpt.edu.vn>
2022-11-30 15:45:02 +0700

    [view]update_password - update class

--- view/update_password.php
+++ view/update_password.php
@@ -1,5 +1,6 @@
+<div class="row m-4">
 <form class="col-6 offset-3  bg-light rounded p-4" action="index.php?act=update_password" method="post">
-        <h1 class="text-center">Quên mật khẩu</h1>
+        <h1 class="text-center font-thu-phap">Cập nhật mật khẩu</h1>
         <input class="col form-control m-2" type="text" name="code" placeholder="Mã code">
         <input class="col form-control m-2" type="password" name="new_password" placeholder="Mật khẩu mới">
         <input class="col form-control m-2" type="password" name="renew_password" placeholder="Nhập lại mật khẩu mới">
@@ -8,3 +9,4 @@
         </center>
 
 </form>
+</div>
