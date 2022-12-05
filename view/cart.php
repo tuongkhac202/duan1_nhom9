0b0f9048b23eb6849d0d41cad0977974a267b069
tuongkhac202 <trankhactuong202@gmail.com>
2022-12-05 15:58:40 +0700

    [front-end] layout Pay page

--- view/cart.php
+++ view/cart.php
@@ -1,18 +1,26 @@
 <article>
+    <center>
+        <h2 class="text-danger font-thu-phap fw-bold m-3 p-0">Tour Của  Qúy Khách</h2>
+    </center>
     <div class="w-75 border border-primaryk m-5 d-block mx-auto">
-        <div class="row">
+        <form action="index.php?act=thanhToan" method="post" class="row">
             <div class="col-6">
                 <div class="py-2 m-3 bg-light">
                     <h3 class="text-center color-main ">Thông tin người đặt</h3>
                 </div>
-                <div class="py-2 m-3 bg-light px-4 py-5"> <br><input class="rounded form-control" type="text"
-                        placeholder="Họ và tên">
-                    <br><input class="rounded  form-control" type="phone" placeholder="Số điện thoại">
-                    <br><input class="rounded  form-control" type="email" placeholder="Email">
-                    <br><input class="rounded  form-control" type="text" placeholder="Mã giảm giá">
+                <div class="py-2 m-3 bg-light px-4 py-5"> <br><input class="rounded form-control" type="text" name="namee" placeholder="Họ và tên" required>
                     <br>
-                    <input class="" type="radio" value="" name="thanhToan" > Thanh toán tại văn phòng
-                    <br> <input class="" type="radio" value="" name="thanhToan"> Chuyển khoản
+                    <input class="rounded  form-control" type="phone" placeholder="Số điện thoại" name="phone" required>
+                    <br>
+                    <input class="rounded  form-control" type="email" placeholder="Email" name="email" required>
+                    <br>
+                    <input class="rounded  form-control" type="text" placeholder="Địa chỉ" name="address" required>
+                    <br>
+                    <input class="rounded  form-control" type="text" placeholder="Mã giảm giá" name="maGiamGia">
+                    <br>
+                    <input class="" type="radio" value="Thanh toán tại văn phòng" name="thanhToann" required> Thanh toán tại văn phòng
+                    <br> 
+                    <input class="" type="radio" value="Chuyển khoản" name="thanhToann"> Chuyển khoản
                 </div>
             </div>
 
@@ -43,7 +51,7 @@
                                                 <br>
                                                 <b >Giá tour:  <b class="text-danger giatien">' . number_format($price, 0, '', '.') . ' VNĐ</b></b>
                                                 <br>
-                                                <b>Trạng thái: ' . $status . '</b>
+                                                <b>Trạng thái: <b class="text-danger">' . $status . '</b></b>
                                                 <br>
                                                 <br>
                                                 <center><a href="index.php?act=xoaBookingCart&idBook=' . $id_booking . '" class="xoabtn btn btn-danger"><i class="fas fa-trash-alt"></i></a></center>
@@ -54,12 +62,12 @@
                                         echo '
                                         <h4 class="text-danger">Tổng tiền: ' . number_format($tongtien, 0, '', '.') . ' VNĐ</b></h4>
                                        <center>
-                                        <a href="index.php?act=thanhToan"><button type="button" class=" btn btn-success m-5">Thanh toán</button></a>                                       
+                                        <button type="submit" class=" btn btn-success m-5">Thanh toán</button>                                      
                                        </center>
                                        ';
                                 }
                                 ?>
             </div>
-        </div>
+        </form>
     </div>
 </article>
--- view/pay.php
+++ view/pay.php
@@ -1,76 +1,98 @@
 <article>
-    <div class="w-75 border border-primaryk m-5 d-block mx-auto">
-        <div class="row">
-            <div class="col-6 offset-3">
-                <div class="py-2 m-3 bg-light">
-                    <h3 class="text-center color-main ">Thông tin người đặt</h3>
-                </div>
-                <div class="py-2 m-3 bg-light px-4 py-5"> <br><input class="rounded form-control" type="text"
-                        placeholder="Họ và tên">
-                    <br><input class="rounded  form-control" type="phone" placeholder="Số điện thoại">
-                    <br><input class="rounded  form-control" type="email" placeholder="Email">
-                    <br><input class="rounded  form-control" type="text" placeholder="Mã giảm giá">
-                    <br>
-                    <input class="" type="radio" value="Thanh toán tại văn phòng" name="thanhToan" > Thanh toán tại văn phòng
-                    <!-- mã qr -->
-                   
-                </div>
-            </div>
 
-            <div class="col-6 offset-3">
-                <div class="py-2 m-3 bg-light">
-                    <h3 class="text-center color-main">Thông tin dịch vụ</h3>
+    <?php
+    $phuongThucThanhToan = $_POST['thanhToann'];
+    $tongtien = 0;
+    foreach ($loadbooking as $cart) :
+        extract($cart);
+        $tongtien += (float)$price;
+    ?>
+        <div class="w-75 border border-primaryk m-5 d-block mx-auto">
+            <form action="index.php?act=confirmPay" method="post" class="row">
+                <input type="text" hidden name="maBook" value="<?= $id_booking ?>">
+                <center>
+                    <h2 class="text-danger font-thu-phap fw-bold my-2">Chi Tiết Thanh Toán</h2>
+                </center>
+                <div class="col-8 offset-2">
+                    <table class="table table-hover table-responsive table-border-1 table-bordered">
+                        <thead class="bg-success text-white">
+                            <th colspan="2">Thông tin khách hàng</th>
+                        </thead>
+                        <tbody class="table-light">
+                        <tr>
+                                <td>Mã khách hàng: </td>
+                                <td><?= $_SESSION['user']['id_customer'] ?></td>
+                            </tr>
+                            <tr>
+                                <td>Họ và tên khách hàng: </td>
+                                <td><?= $_POST['namee'] ?></td>
+                            </tr>
+                            <tr>
+                                <td>Số điện thoại: </td>
+                                <td><?= $_POST['phone'] ?></td>
+                            </tr>
+                            <tr>
+                            <td>Email: </td>
+                                <td><?= $_POST['email'] ?></td>
+                            </tr>
+                            <td>Địa chỉ: </td>
+                                <td><?= $_POST['address'] ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <br>
+                    <table class="table table-hover table-responsive table-border-1 table-bordered">
+                        <thead class="bg-success text-white">
+                            <th colspan="2">Thông tin dịch vụ</th>
+                           
+                        </thead>
+                        <tbody class="table-light">
+                        <tr>
+                                <td>Mã dịch vụ: </td>
+                                <td><?= $id_tour?></td>
+                            </tr>
+                            <tr>
+                                <td><?= $name_tour ?></td>
+                                <td><?= number_format($price, 0, '', ',') ?> vnđ</td>
+                            </tr>
+                            <tr>
+                                <td>Giảm giá: </td>
+                                <td>0 </td>
+                            </tr>
+                            <tr>
+                                <td>Phương thức thanh toán: </td>
+                                <td><?= $phuongThucThanhToan ?></td>
+                            </tr>
+                            <tr>
+                                <td class="fw-bold">Tổng cộng: </td>
+                                <td class="text-danger fw-bold"><?= number_format($tongtien, 0, '', ',') ?> VNĐ</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                <?php endforeach ?>
+                <b>Địa chỉ văn phòng: </b>
+                <p>Công ty du lịch Yến Tường, Tòa nhà 91B, Phường An Khánh, Quận Ninh Kiều, TP. Cần Thơ, Việt Nam.</p>
+                <b>Thông tin chuyển khoản: </b>
+                <br>
+                <p>NGÂN HÀNG: TECHCOMBANK <br> SỐ TÀI KHOẢN: 09876543212345678 <br> CHỦ TÀI KHOẢN: YEN TUONG COMPANY </p>
+                <b>Xin quý khách vui lòng chuyển khoản theo cú pháp: </b>
+                <p>TEN KHACH HANG: ... - MA SO KHACH HANG:... - MA SO DICH VU:...
+                <br>
+                VÍ DỤ: NGUYỄN VĂN THỦM - MÃ SỐ KHÁCH HÀNG: 001 - MÃ SỐ DỊCH VỤ: 22
+                </p>
+                <!-- <div class="qrcode">
+                    <b>Hoặc quét mã QR bên dưới để thanh toán</b>
+                    <br>
+                    <br>
+                    <center>
+                        <img src="upload/qrcode_41113624_a2bae233cab714d88f39994ad3394fbc.png" alt="" width="50%" height="">
+                    </center>
+                </div> -->
+                <center>
+                    <a href="index.php?act=cart"><button type="button" class=" btn btn-danger m-5">Quay lại giỏ hàng</button></a>
+                    <button type="submit" class=" btn btn-success m-5">Xác nhận đã thanh toán</button>
+                </center>
                 </div>
-                <?php
-                                if (isset($_SESSION['user'])) {
-                                        $tongtien = 0;
-                                        foreach ($loadbooking as $cart) {
-                                                extract($cart);
-                                                $tongtien += (float)$price;
-                                                $hinh_tour = "upload/" . $image_tour;
-                                        echo '
-                                                <div class="m-3">
-                                                <img class="rounded w-100" src="' . $hinh_tour . '" alt="" height="300">
-                                                <h6 class="text-danger">' . $name_tour . '</h6>
-                                                <b>Ngày khởi hành: ' . $day_start . '</b>
-                                                <br>
-                                                <b>Số người: </b>
-                                                <input readonly class=" w-25 form-control" type="number" value="' . $number_customers . '">
-                                                <b>Ghi chú: </b>
-                                                <br>
-                                                <textarea readonly class="w-100" name="" id="" cols="30" rows="5">
-                                                ' . $note . '
-                                                </textarea>
-                                                <br>
-                                                <b >Giá tour:  <b class="text-danger giatien">' . number_format($price, 0, '', '.') . ' VNĐ</b></b>
-                                                <br>
-                                                <hr>
-                                                </div>
-                                        ';
-                                        }
-                                        echo '
-                                        <h4 class="text-danger">Tổng tiền: ' . number_format($tongtien, 0, '', '.') . ' VNĐ</b></h4>
-                                        <b>Địa chỉ văn phòng: </b>
-                                        <p>Công ty Yến Tường, Tòa nhà 91B, Phường An Khánh, Quận Ninh Kiều, TP. Cần Thơ, Việt Nam.</p>
-                                        <b>Thông tin chuyển khoản: </b>
-                                        <br>
-                                        <p>NGÂN HÀNG: TECHCOMBANK <br> SỐ TÀI KHOẢN: 09876543212345678 <br> CHỦ TÀI KHOẢN: YEN TUONG COMPANY  </p>
-                                        <div class="qrcode">
-                                        <b>Hoặc quét mã QR bên dưới để thanh toán</b>
-                                        <br>
-                                        <br>
-                                        <center>
-                                        <img src="upload/qrcode_41113624_a2bae233cab714d88f39994ad3394fbc.png" alt="" width="50%" height="">
-                                        </center>
-                                        </div>
-                                       <center>
-                                       <a href="index.php?act=cart"><button type="button" class=" btn btn-danger m-5">Quay lại giỏ hàng</button></a>                                       
-                                        <a href="index.php?act=thanhToan"><button type="button" class=" btn btn-success m-5">Xác nhận đã thanh toán</button></a>                                       
-                                       </center>
-                                       ';
-                                }
-                                ?>
             </div>
         </div>
-    </div>
 </article>
